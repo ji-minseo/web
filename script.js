2c09f6e3a8d66cd8f8a97988fa9cf43d60b40c6f
서지민 <seojimin@seojimicBookAir.lan>
2021-03-06 03:47:59 +0900

    add folder

--- script.js
+++ script.js
@@ -13,30 +13,31 @@ this.select();
 }); 
 
 window.onload = function () {
-    randomObject();
+    // randomObject();
+    console.log("hhi");
 }
 
 
 function randomObject() {
-  let posx = Math.random() * (700 - 50) + 50;
-  let posy = Math.random() * (700 - 50) + 50;
-  console.log(posx);
-  let temp = document.getElementsByClassName("window");
+  // let posx = Math.random() * (700 - 50) + 50;
+  // let posy = Math.random() * (700 - 50) + 50;
+  // console.log(posx);
+  let temp = document.getElementsByClassName("folder");
   // for(let i = 0; i < temp.length; i++) {
   //   temp[i].style.left = posx +"px";
   //   temp[i].style.top = posy +"px";
   //   posx = Math.random() * (700 - 50) + 50;
   //   posy = Math.random() * (700 - 50) + 50;
   // }
-  temp[0].style.left = Math.random() * (90 - 10) + 0 +"vh";
-  temp[0].style.top = Math.random() * (100 - 0) + 0 +"vw";
+  temp[0].style.left = Math.random() * (10 - 0) + 0 +"%";
+  temp[0].style.top = Math.random() * (10 - 0) + 0  +"vh";
 
-  temp[1].style.right = Math.random() * (80 - 20) + 0 +"vh";
-  temp[1].style.bottom = Math.random() * (90 - 10) + 0 +"vw";
+  temp[1].style.left = Math.random() * (10 - 0) + 0 +"%";
+  temp[1].style.top = Math.random() * (40 - 30) + 40  +"vh";
 
 
-  temp[2].style.right = Math.random() * (70 - 30) + 0 +"vh";
-  temp[2].style.bottom = Math.random() * (80 - 20) + 0 +"vw";
+  temp[2].style.left = Math.random() * (10 - 0) + 0+"%";
+  temp[2].style.top = Math.random() * (70 - 60) + 60  +"vh";
 
 
 
